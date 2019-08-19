ed986697333a856875ec0248b230a7137976012f
songjia <songjia@mucfc.com>
2019-08-19 18:06:22 +0800

    puzzle

--- algs4-I/4.SliderPuzzle/Board.java
+++ algs4-I/4.SliderPuzzle/Board.java
@@ -17,15 +17,15 @@ public class Board {
 
     // string representation of this board
     public String toString() {
-        String result = '';
+        String result = "";
         result += String.valueOf(size);
-        result += '\n';
+        result += "\n";
         for (int i = 0; i < size; i++) {
             for (int j = o; j < size; j++) {
                 result += tiles[i][j];
-                result += ' ';
+                result += " ";
             }
-            result += '\n';
+            result += "\n";
         }
         return result;
     }
@@ -90,7 +90,7 @@ public class Board {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (tiles[i][j] != y[i][j]) {
-                    return false
+                    return false;
                 }
             }
         }
@@ -98,12 +98,72 @@ public class Board {
     }
 
     // all neighboring boards
-    public Iterable<Board> neighbors()
+    public Iterable<Board> neighbors() {
+        Queue<Board> result = new Queue<Board>();
+        int[][] copy = new int[size][size];
+        int x;
+        int y;
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (tiles[i][j] == 0) {
+                    x = i;
+                    y = j;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+        if (x > 0) {
+            result.enqueue(swap(x, y, x - 1, y));
+        }
+        if (x < size - 1) {
+            result.enqueue(swap(x, y, x + 1, y));
+        }
+        if (y > 0) {
+            result.enqueue(swap(x, y, x, y - 1));
+        }
+        if (y < size - 1) {
+            result.enqueue(swap(x, y, x, y + 1));
+        }
+        return result;
+    }
+
+    private Board swap(int x0, int y0, int x1, int y1) {
+        int[][] copy = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == x0,j = y0){
+                    copy[i][j] = tiles[x1][y1];
+                }else if (i == x1,j == y1){
+                    copy[i][j] = tiles[x0][y0];
+                }else{
+                    copy[i][j] = tiles[i][j];
+                }
+            }
+        }
+        return new Board(copy);
+    }
 
     // a board that is obtained by exchanging any pair of tiles
-    public Board twin()
+    public Board twin() {
+        if (tiles[0][0] == 0 || tiles[0][1] == 0) {
+            return new Board(swap(size - 1, 0, size - 1, 1))
+        } else {
+            return new Board(swap(0, 0, 0, 1))
+        }
+    }
 
     // unit testing (not graded)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        int[][] data = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        }
+        Board board = new Board(data);
+        StdOut.print(board.toString());
+    }
 
 }
